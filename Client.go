eb39320d5bcbfb363775a9c902d7a1f775ef42b0
jviguy <jviguytwo2@gmail.com>
2020-11-04 19:33:45 -0500

    add 2020 live data

---
+++ Client.go
@@ -0,0 +1,68 @@
+package Elector
+import (
+	"encoding/json"
+	"github.com/gojektech/heimdall/v6/httpclient"
+	"io/ioutil"
+	"time"
+)
+
+type Client struct {
+	http *httpclient.Client
+}
+
+type Result struct {
+	President struct {
+		CalledWinner string `json:"called-winner"`
+		CalledDemocrats int `json:"called-d"`
+		CalledRepublican int `json:"called-r"`
+		LeanDemocrat int `json:"lean-d"`
+		LeanRepublican int `json:"lean-r"`
+		LikelyDemocrat int `json:"likely-d"`
+		LikelyRepublican int `json:"likely-r"`
+		SolidDemocrat int `json:"solid-d"`
+		SolidRepublican int `json:"sold-r"`
+		Tossup int `json:"toss-up"`
+	} `json:"president"`
+	Senate struct {
+		CalledWinner string `json:"called-winner"`
+		SeatedDemocrats int `json:"seated-d"`
+		SeatedRepublicans int `json:"seated-r"`
+		CalledDemocrats int `json:"called-d"`
+		CalledRepublican int `json:"called-r"`
+		LeanDemocrat int `json:"lean-d"`
+		LeanRepublican int `json:"lean-r"`
+		LikelyDemocrat int `json:"likely-d"`
+		LikelyRepublican int `json:"likely-r"`
+		SolidDemocrat int `json:"solid-d"`
+		SolidRepublican int `json:"sold-r"`
+		Tossup int `json:"toss-up"`
+	} `json:"senate"`
+	House struct {
+		CalledWinner string `json:"called-winner"`
+		CalledDemocrats int `json:"called-d"`
+		CalledRepublican int `json:"called-r"`
+		LeanDemocrat int `json:"lean-d"`
+		LeanRepublican int `json:"lean-r"`
+		LikelyDemocrat int `json:"likely-d"`
+		LikelyRepublican int `json:"likely-r"`
+		SolidDemocrat int `json:"solid-d"`
+		SolidRepublican int `json:"sold-r"`
+		Tossup int `json:"toss-up"`
+	} `json:"house"`
+}
+
+func New(timeout time.Duration) *Client {
+	return &Client{http: httpclient.NewClient(httpclient.WithHTTPTimeout(timeout))}
+}
+
+func (c Client) GetData(year int) (*Result,error) {
+	res,err := c.http.Get("https://www.politico.com/2020-national-results/balance-of-power.json",nil)
+	if err != nil{
+		return nil,err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	println(string(body))
+	data := &Result{}
+	err = json.Unmarshal(body,data)
+	return data , err
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Jviguy/Elector
+
+go 1.14
+
+require github.com/gojektech/heimdall/v6 v6.1.0
